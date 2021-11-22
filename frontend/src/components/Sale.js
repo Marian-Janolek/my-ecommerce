55e3387fe6ba67ee0df68329b48fa17fe0bdf372
Marian-Janolek <m.janolek@gmail.com>
2021-11-22 17:12:10 +0100

    add swiper

--- frontend/src/components/Sale.js
+++ frontend/src/components/Sale.js
@@ -3,7 +3,10 @@ import styled from 'styled-components';
 import Card from './Card';
 import { useSubmenuContext } from '../context/submenuContext';
 import { useProductContext } from '../context/productsContext';
-
+import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
+import 'swiper/swiper-bundle.min.css';
+import SwiperCore, { Navigation, Pagination } from 'swiper';
+SwiperCore.use([Navigation, Pagination]);
 const Sale = () => {
   const { closeSubmenu } = useSubmenuContext();
   const {
@@ -17,22 +20,44 @@ const Sale = () => {
       <Wrapper className="section-center">
         <h2>Sale</h2>
         <div className="underline"></div>
-        <div className="new-card">
+
+        <Swiper
+          spaceBetween={30}
+          navigation
+          pagination={{ clickable: true }}
+          breakpoints={{
+            300: {
+              spaceBetween: 16,
+              slidesPerView: 1,
+            },
+            400: {
+              slidesPerView: 2,
+            },
+            600: {
+              slidesPerView: 3,
+            },
+            900: {
+              slidesPerView: 4,
+            },
+          }}
+        >
           {sale_products.map((sale) => {
             const { _id, name, image, price } = sale;
             return (
-              <Card
-                key={_id}
-                id={_id}
-                name={name}
-                image={image}
-                price={price}
-                data={sale.sizes}
-                text="sale"
-              />
+              <SwiperSlide key={_id}>
+                <Card
+                  key={_id}
+                  id={_id}
+                  name={name}
+                  image={image}
+                  price={price}
+                  data={sale.sizes}
+                  text="sale"
+                />
+              </SwiperSlide>
             );
           })}
-        </div>
+        </Swiper>
       </Wrapper>
     </div>
   );
@@ -53,6 +78,15 @@ const Wrapper = styled.div`
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(220px, 1fr));
     grid-gap: 2rem;
+  }
+  .swiper-pagination {
+    bottom: 0px;
+  }
+  .swiper {
+    padding: 0rem 0rem 2rem;
+  }
+  .swiper-pagination-bullet-active {
+    background-color: var(--dark-color);
   }
 `;
 
